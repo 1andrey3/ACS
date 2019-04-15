729f7c83dfe8b97591ca19a40c79e50449e66dbd
Jorge Tinoco <iorifonseca777@gmail.com>
2019-04-15 16:34:04 -0500

    requerimiento #5 terminado

--- application/views/grupoVM.php
+++ application/views/grupoVM.php
@@ -74,7 +74,7 @@
       <th>Hora De Apertura</th>
       <th>Ingeniero Creador De G</th>
       <th>Incidente</th>
-      <th>Tomar datos</th>
+      <th>Tomar Datos</th>
     </tr>
   </thead>
   <tbody>
@@ -92,7 +92,7 @@
         <td id ="ths"> <?php echo $row['Hora_apertura'] ?></td>
         <td id ="ths"> <?php echo $row['Ingeniero_CreadorG'] ?></td>
         <td id ="ths"> <?php echo $row['Incidente'] ?></td>
-        <td><button class="btn btn-info" id="btn" data-toggle="modal" data-target="#myModal" onclick ="tomarDatos(this.parentNode)">tomar</button></td>
+        <td><button class="btn btn-info" data-toggle="modal" data-target="#myModal" id ="tomarDatos">tomar</button></td>
       </tr>
     <?php endforeach ?>
   </tbody>
@@ -127,7 +127,7 @@
             <label for="">ESTACIÓN:</label><br>
             <select class="form-control"  name="estacion" id ="estacion">
             <?php foreach($estacion as $key=>$row):?>
-            <option id ="posicion" value="<?php echo $key ?>"><?php echo $row['sitio']?> </option>
+            <option id ="posicion" value="<?php echo $key+1 ?>"><?php echo $row['sitio']?> </option>
             <?php endforeach ?>
           </select>
           </div>
@@ -157,7 +157,7 @@
           </div>
           <div class="form-group">
             <label for="">Regional Skype</label><br>
-            <input type="text" class="form-control rsky" name="rSkype" id="rSkype" disabled>
+            <input type="text" class="form-control rsky" name="rSkype" id="rSkype">
           </select>
           </div>
           <div>
@@ -193,58 +193,64 @@
   $(document).ready(function() {
     $('#tablaForm').DataTable( {
     } );
+
   }); 
-  
-const estacionTabla = fetch('tablaJson', {    
-}).then(data => data.text())
-  .then(data => {
-    const btn = document.querySelector('#formulario');
-    const obj  = JSON.parse(data);
-    //console.log(btn);
-    console.log(obj);
-    btn.addEventListener('click', () => {
+//llamado a la base de datos
+(async function load(){
+  async function baseDatos(url){
+    const base = await fetch(url).then( response => response.text());
+    const data = await JSON.parse(base);
+    return data;
+  }
+  // llamado a las tablas
+  const objEstacion = await baseDatos('tablaJson');
+
+  //boton Formulario
+  const btn = document.querySelector('#formulario');
+  btn.addEventListener('click', () => {
       const estacion = document.querySelector('#estacion');
       console.log(estacion);
       estacion.addEventListener('click', () => {
-        console.log('holas');
         const estacion2 = estacion.value;
         console.log(estacion2);
-        for(let x = 0; x < obj.length; x++){
-          if(obj[x].id_estacion == estacion2){
-            document.getElementById('rSkype').value = obj[x].region;
+        for(let x = 0; x < objEstacion.length; x++){
+          if(objEstacion[x].id_estacion == estacion2){
+            document.getElementById('rSkype').value = objEstacion[x].region;
           }
         }
+      });
+    });
+})();
+  //botones 'tomar'
+    const tomarDatos = document.querySelectorAll('#tomarDatos');
+    for(tomar of tomarDatos){
+      tomar.addEventListener('click', (evt) => {
+        const boton = evt.target;
+        console.log(boton.innerHTML);
+        const d = boton.parentNode;
+        const x = d.parentNode;
+        console.log(x.childNodes[5].innerHTML);
+
+        // enviar datos de la tabla a el modal
+        //  document.getElementById('idSiteAccess').value = parseInt(x.childNodes[1].innerHTML);
+        document.getElementById('fechaSolicitud').value = x.childNodes[3].innerHTML;
+        let estacionCampo = document.getElementById('estacion').value = x.childNodes[5].innerHTML;
+        let tecnologia = document.getElementById('tecnologia').value = parseInt(x.childNodes[7].innerHTML);
+        let banda = document.getElementById('banda').value = parseInt(x.childNodes[9].innerHTML);
+        document.getElementById('enteEjecutor').value = x.childNodes[11].innerHTML;
+        document.getElementById('nGSkype').value = x.childNodes[13].innerHTML;
+        document.getElementById('rSkype').value = x.childNodes[15].innerHTML;
+        document.getElementById('personaSolicita').value = x.childNodes[17].innerHTML;
+        document.getElementById('horaApertura').value = x.childNodes[19].innerHTML;
+        document.getElementById('ingenieroCreadorG').value = x.childNodes[21].innerHTML;
+        document.getElementById('incidente').value = x.childNodes[23].innerHTML;
+
+        $("#estacion option[value="+ estacionCampo +"]").attr("selected",true);
+        $("#banda option[value="+ banda +"]").attr("selected",true);
+        $("#tecnologia option[value="+ tecnologia +"]").attr("selected",true);
+        const estacion = document.querySelector('#estacion');
+        console.log(x.childNodes[5].innerHTML);
       })
-    })
-  })
-  .catch(error => {
-    console.log('ERROR' + error);
-  });
-
-  function tomarDatos(a){
-    const x = a.parentNode;
-    console.log(x.childNodes[5].innerHTML);
-    //  document.getElementById('idSiteAccess').value = parseInt(x.childNodes[1].innerHTML);
-    document.getElementById('fechaSolicitud').value = x.childNodes[3].innerHTML;
-    document.getElementById('estacion').value = x.childNodes[5].innerHTML;
-    let tecnologia = document.getElementById('tecnologia').value = parseInt(x.childNodes[7].innerHTML);
-    let banda = document.getElementById('banda').value = parseInt(x.childNodes[9].innerHTML);
-    document.getElementById('enteEjecutor').value = x.childNodes[11].innerHTML;
-    document.getElementById('nGSkype').value = x.childNodes[13].innerHTML;
-    document.getElementById('personaSolicita').value = x.childNodes[17].innerHTML;
-    document.getElementById('horaApertura').value = x.childNodes[19].innerHTML;
-    document.getElementById('ingenieroCreadorG').value = x.childNodes[21].innerHTML;
-    document.getElementById('incidente').value = x.childNodes[23].innerHTML;
-
-    $("#banda option[value="+ banda +"]").attr("selected",true);
-    $("#tecnologia option[value="+ tecnologia +"]").attr("selected",true);
-
-    // for(let x = 0; x < obj.length; x++){
-    //   if(obj[x].sitio == estacion){
-    //     var posicionEstacion = obj[x].id_estacion;
-    //     break;
-    //   }
-    // }
-    // console.log(posicionEstacion);
-  }
+    }
+    console.log(tomarDatos);
 </script>
