ca51f383be8d05a9dedb763d6d8d2fa24762a518
unknown <jfchaparro33@misena.edu.co>
2019-05-02 10:20:03 -0500

    integrar el proceso de ACS se comiensa la integracion y revision del proceso de ACS para el issue #24

--- application/models/Formulario.php
+++ application/models/Formulario.php
@@ -6,20 +6,35 @@ class Formulario extends CI_Model{
 
     }
     function recorridoEstacion(){
-         $data= $this->db->query('SELECT * FROM estacion');
+         $data= $this->db->query('SELECT id_estacion, sitio, region, departamento, municipio FROM estacion');
          return $data->result_array();
     }
     function recorridoTecnologia(){
-        $data= $this->db->query('SELECT * FROM tecnologia');
-         return $data->result_array(); 
+        $data= $this->db->query('SELECT id_tecnologia, nombre_tecnologia FROM tecnologia');
+         return $data->result_array();
     }
     function recorridoBanda(){
-        $data= $this->db->query('SELECT * FROM  banda');
-        return $data->result_array(); 
+        $data= $this->db->query('SELECT id_banda, nombre_banda FROM banda');
+        return $data->result_array();
     }
     function recorridoGrupoVM(){
-        $data= $this->db->query('SELECT * FROM  grupovm');
-        return $data->result_array(); 
+        $data= $this->db->query('
+            SELECT id_vm_zte,
+                ID_Site_Access,
+                F_H_Solicitud,
+                Estacion,
+                Tecnologia,
+                Banda,
+                Ente_ejecutor,
+                Nombre_grupo_skype,
+                Regional_skype,
+                Persona_solicita,
+                Hora_apertura,
+                Ingeniero_CreadorG,
+                Incidente,
+                hora_solicitud
+            FROM sitio');
+        return $data->result_array();
     }
     function guardarDatos($data){
         var_dump($data);
